61b9175937857a46c91392bc015b02045ef6010f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-20 19:02:53 +0100

    Add ball controller

---
+++ Assets/scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class BallController : MonoBehaviour {
+
+	[SerializeField]
+	private float force = 1;
+
+	private Rigidbody rb;
+
+	private void Awake () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	private void FixedUpdate () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		rb.AddForce (Vector3.forward * force * h);
+		rb.AddForce (Vector3.right * force * -v);
+
+	}
+}
