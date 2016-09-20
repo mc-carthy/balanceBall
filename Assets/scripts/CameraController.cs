0e23b444cdad5c4ec06a0f89f20034f96f72d66d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-20 19:08:16 +0100

    Add camera controller

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject ball;
+	private Vector3 offset;
+
+	private void Awake () {
+		ball = GameObject.FindGameObjectWithTag ("Player");
+		offset = ball.transform.position - transform.position;
+	}
+
+	private void Update () {
+		if (ball.transform.position.y > 0) {
+			transform.position = ball.transform.position - offset;
+		}
+	}
+}
